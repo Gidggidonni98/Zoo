50bfdb10d38b7ce7d07b196a2753d2c647243145
GaticaNahum <20203tn058@utez.edu.mx>
2022-04-22 21:54:56 -0500

    Especies-Tabla
    Tabla de las especies

--- ZooFront/especies.js
+++ ZooFront/especies.js
@@ -1,4 +1,5 @@
 const url = "http://localhost:8080/api/especie";
+const urlEspecies = "http://localhost:8080/api/especie";
 
 //-----------------------REGISTRAR ESPECIE----------------------------------//
 
@@ -31,3 +32,31 @@ const registerEspecie = () => {
     document.getElementById("nombre_comun").value = "";
   });
 };
+
+
+const findEspecies = async() => {
+    await $.ajax({
+        method: 'GET',
+        headers: { "Accept": "application/json" },
+        url: urlEspecies + '/'
+    }).done(function(res) {
+        content = "";
+        res = res.data;
+        for (let i = 0; i < res.length; i++) {
+            content += `
+            <tr class="text-center">
+                <td>${res[i].id}</td>
+                <td>${res[i].familia}</td>
+                <td>${res[i].nombreCientifico} </td>
+                <td>${res[i].nombreComun}</td>
+                <td>${res[i].peligroExtincion}</td>
+                <td>
+                    <button class='btn btn-warning' data-toggle='modal' onclick='getInfoClient()' data-target='#detallesCliente'><i class="fa-solid fa-pen-to-square"></i></button>
+                </td>
+            </tr>
+                `;
+        };
+        $("#table2 > tbody").html(content)
+    });
+};
+findEspecies();
